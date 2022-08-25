537590049937b15d7cf49277dbd6607bb7919504
donovan2121 <gilchrist068@gmail.com>
2022-08-26 01:56:24 +0800

    test null

--- public/js/counter.js
+++ public/js/counter.js
@@ -6,32 +6,37 @@ const apiUrl = 'https://1xwja6qasb.execute-api.us-east-1.amazonaws.com/prod/visi
 
 
 function getVisitors(){
-    fetch(apiUrl + "?id=101")
+
+    let count;
+    let i=1;
+    let visitorCount = document.createElement('span');
+
+    try{
+        fetch(apiUrl + "?id=101")
         .then((response) => {
             return response.json();
         })
         .then((data) => {
             let visitors = data;
             console.log(data);
-            let count;
-            let i=1;
-            let visitorCount = document.createElement('span');
-            if (visitors.visitorCount != null){
+            
+            if (visitors.visitorCount){
                 count = visitors.visitorCount + 1;
                 visitorCount.innerText = `${count}`;
                 visitor.appendChild(visitorCount);
                 updateVisitors(count);
             }
-            else {
-                if (i == 1){
-                    postVisitors();
-                    i++;
-                    getVisitors();
-                    visitorCount.innerText = `30`;
-                    visitor.appendChild(visitorCount);
-                }
-            } 
-            })
+        })
+
+    } catch (error) {
+        if (i == 1){
+            postVisitors();
+            i++;
+            visitorCount.innerText = `30`;
+            visitor.appendChild(visitorCount);
+        }     
+    }
+      
 }
 
 function postVisitors(){
